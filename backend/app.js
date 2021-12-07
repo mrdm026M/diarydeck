cf423f07fff686c78036cbf58b44a8ac2951f3a1
mrdm026M <mrdm026@gmail.com>
2021-12-07 22:52:12 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,8 @@
 const express = require("express");
-// const notes = require("./data/notes");
 const { notFound, errorHandler } = require("./middleware/error");
 
 const app = express();
 app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("API is running");
-// });
-
-// app.get("/api/v1/notes", (req, res) => {
-//   res.json(notes);
-// });
 
 // Route Imports
 const users = require("./routes/userRoute");
@@ -19,6 +10,9 @@ app.use("/api/v1", users);
 
 const notes = require("./routes/noteRoute");
 app.use("/api/v1/", notes);
+
+const todos = require("./routes/todoRoute");
+app.use("/api/v1/", todos);
 
 // Middleware for Errors
 app.use(notFound);
