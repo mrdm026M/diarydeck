185ec9c27a0e99e77aadc2a4e59284a05ee24fba
mrdm026M <mrdm026@gmail.com>
2021-12-07 22:58:21 +0530

    updated title

--- frontend/src/pages/Dashboardpage.js
+++ frontend/src/pages/Dashboardpage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import DashboardMain from "../components/Dashboard/DashboardMain";
+import Metadata from "../Metadata";
 
 const Dashboardpage = () => {
   return (
     <>
+      <Metadata title="Minor Project - Dashboard" />
       <DashboardMain />
     </>
   );
--- frontend/src/pages/Notes/NotesPage.js
+++ frontend/src/pages/Notes/NotesPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Metadata from "../../Metadata";
 import NotesMain from "../../components/Notes/NotesMain";
 
 const NotesPage = () => {
   return (
     <>
+      <Metadata title="Minor Project - Notes" />
       <NotesMain />
     </>
   );
